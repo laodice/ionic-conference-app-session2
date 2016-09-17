5bc38467eff20c4d98944274ba13c359e772afcf
laodice <laodice.melliti@telecom-st-etienne.fr>
2016-09-17 14:15:17 +0200

    Add button to signup for a presentation

--- app/pages/session-detail/session-detail.ts
+++ app/pages/session-detail/session-detail.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
 
+import { UserData } from '../../providers/user-data';
+
 
 @Component({
   templateUrl: 'build/pages/session-detail/session-detail.html'
 })
 export class SessionDetailPage {
   session: any;
+  showParticipate = false;
 
-  constructor(public navParams: NavParams) {
+  constructor(
+    public navParams: NavParams,
+    public userData: UserData
+  ) {
     this.session = navParams.data;
+    
+    this.userData.hasLoggedIn().then((hasLoggedIn) => {
+      this.showParticipate = hasLoggedIn === 'true';
+    });
+  }
+  
+  addParticipant() {
+    console.log('Clicked participate');
   }
 }
